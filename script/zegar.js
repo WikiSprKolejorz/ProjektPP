b919b5e0668260f5961cf1fa40fe7243fe887571
Adam Praca <aspring9999@gmail.com>
2023-05-22 10:56:22 +0200

    Stoper nie restartuje sie przy przechodzeniu na podstrony- uzycie localstorage

--- script/zegar.js
+++ script/zegar.js
@@ -6,7 +6,7 @@ function zegar(){
     let minuta = czasTeraz.getMinutes();
     let sekunda = czasTeraz.getSeconds();
     let czasWartosc = (godzina>12)? godzina-12 : godzina;
-    czasWartosc = (godzina==0)? 12 : godzina-12;
+    //czasWartosc = (godzina==0)? 12 : godzina-12;
     czasWartosc += (minuta<=9)? ":0" + minuta : ":" + minuta;
     czasWartosc += (sekunda<=9)? ":0" + sekunda : ":"+ sekunda;
     czasWartosc += (godzina > 12)? "PM" : "AM";
@@ -17,24 +17,43 @@ function zegar(){
     setTimeout(zegar,1);      // czasTeraz.toTimeString() zamiast czasWartosc
 }
 
-//stoper
-// s.ms*100   1.1
 let stoper_sek = 0;
 let stoper_ms = 0;
-function stoper() {
-    if(stoper_ms>=9) {stoper_ms=0; stoper_sek++; }
-    else{ stoper_ms++; }
-    const elemeStoper = document.getElementById("Stoper");
-    elemeStoper.innerHTML = "<i>" + stoper_sek + "." + stoper_ms + "</i>";
-
-}
 let stoperID = null;
-function start_stoper(){
-    stoperID = setInterval(stoper, 100);
-    console.log(stoperID);
-}
-function stop_stoper() {
-    clearInterval(stoperID);
 
+function stoper() {
+  if (stoper_ms >= 9) {
+    stoper_ms = 0;
+    stoper_sek++;
+  } else {
+    stoper_ms++;
+  }
+
+  const elemeStoper = document.getElementById("Stoper");
+  elemeStoper.innerHTML = "<i>" + stoper_sek + "." + stoper_ms + "</i>";
 }
-window.onload = function() {zegar(); start_stoper(); }
+
+function start_stoper() {
+  stoperID = setInterval(stoper, 100);
+}
+
+function saveStopwatchState() {
+  localStorage.setItem("stoper_sek", stoper_sek);
+  localStorage.setItem("stoper_ms", stoper_ms);
+}
+
+function loadStopwatchState() {
+  stoper_sek = parseInt(localStorage.getItem("stoper_sek")) || 0;
+  stoper_ms = parseInt(localStorage.getItem("stoper_ms")) || 0;
+}
+
+window.onload = function() {
+  loadStopwatchState();
+  start_stoper();
+};
+
+window.addEventListener("beforeunload", function() {
+  saveStopwatchState();
+});
+
+
