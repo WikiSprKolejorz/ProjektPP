0a8ffe20e387a5af82a038e9ae2ab04634a2d0bb
Adam Praca <aspring9999@gmail.com>
2023-05-24 15:33:52 +0200

    canvas feature added, bouncing ball (odbijające się jądro)

--- script/canvas.js
+++ script/canvas.js
@@ -1,73 +1,36 @@
+const canvas = document.getElementById('grafikaJS');
+const ctx = canvas.getContext('2d');
 
-let canvas, context;
+const p = {
+  x: 25,
+  y: 25
+};
+const velo = 3;
+const corner = 50;
+const rad = 20;
 
-window.onload = ()=>{
-    canvas = document.getElementById("canvas");
-context = canvas.getContext("2d");
+const ball = {
+  x: p.x,
+  y: p.y
+};
+
+let moveX = Math.cos(Math.PI / 180 * corner) * velo;
+let moveY = Math.sin(Math.PI / 180 * corner) * velo;
+
+const DrawMe = () => {
+  ctx.clearRect(0, 0, 400, 300);
+
+  if (ball.x > canvas.width - rad || ball.x < rad) moveX = -moveX;
+  if (ball.y > canvas.height - rad || ball.y < rad) moveY = -moveY;
+
+  ball.x += moveX;
+  ball.y += moveY;
+
+  ctx.beginPath();
+  ctx.fillStyle = 'green';
+  ctx.arc(ball.x, ball.y, rad, 0, Math.PI * 2, false);
+  ctx.fill();
+  ctx.closePath();
 }
 
-let rysujLinie = ()=>{
-    context.beginPath();
-    context.lineWidth=5;
-    context.strokeStyle="blue";
-    context.moveTo(10,10); 
-    context.lineTo(canvas.width-10, canvas.height-10);
-    context.stroke();
-    context.beginPath();
-    context.lineWidth=3;
-    context.strokeStyle= "rgb(0,255,255)";
-    context.moveTo(10,20);
-    context.lineTo(canvas.width-20, canvas.height-10)
-    context.stroke();
-}
-let rysujKrzywa = ()=>{
-  
-    context.strokeStyle="rgb(100,255,100)";
-    context.lineWidth=4;
-    context.beginPath();
-    context.arc(200, 200, 150, Math.PI, 2*Math.PI);
-    context.stroke();
-}
-let rysujFigury =()=>{
-    let dlRamion = 100;
-    let dlPodstawy = 150;
-    let h= Math.sqrt( Math.pow(dlRamion,2)-Math.pow(dlPodstawy/2,2));
-
-    context.beginPath();
-    //A
-    context.moveTo(canvas.width/2-dlPodstawy*0.5,canvas.height/2);
-    //B
-    context.lineTo(canvas.width/2+dlPodstawy*0.5,canvas.height/2);
-    //C
-    context.lineTo(canvas.width/2,canvas.height*0.5-h);
-    //A
-    //context.lineTo(canvas.width/2-dlPodstawy*0.5, canvas.height/2);
-    context.closePath();
-    context.stroke();
-
-    context.fillStyle="green";
-    context.fill();
-
-    context.lineWidth=8;
-    context.lineJoin = "round"; //mitre level
-    context.strokeStyle = "#224422";
-    context.stroke();
-
-    //prostokat
-    context.fillStyle= "rgba(0,0,250,0.5)";
-    context.fillRect(100,150,150,200);
-    context.strokeStyle="rgb(250,0,0)";
-    context.strokeRect(100,150,150,200);
-    //okrag
-    context.beginPath();
-    context.globalAlpha=0.2;
-    context.arc(170,150,70,0,2*Math.PI);
-    context.fillStyle= "rgb(0,250,0)";
-    context.fill();
-    context.strokeStyle="rgb(120,150,100)";
-    context.stroke();
-    context.globalAlpha=1;
-}
-let  czysc= ()=>{
-    context.clearRect(0,0,canvas.width, canvas.height);
-}
+setInterval(DrawMe, 10);
