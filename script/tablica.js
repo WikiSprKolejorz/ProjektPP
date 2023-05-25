d083012ca47757a48baa7384946b08c93b130426
Wikus <wikol123springer@gmail.com>
2023-05-25 02:24:17 +0200

    zrobion

--- script/tablica.js
+++ script/tablica.js
@@ -1,45 +1,45 @@
 //alert();
 //szablon na bazie funkcja
-function samolot(id, model, liczbaPas, klasa, rok, stronaProd)
+function kibice(Lp, druzyna, rok, liga, woj, stronaKlub)
 {
-    this.id = id;
-    this.model = model;
-    this.liczbaPas = liczbaPas;
-    this.klasa = klasa;
+    this.Lp = Lp;
+    this.druzyna = druzyna;
     this.rok = rok;
-    this.stronaProd = stronaProd
+    this.liga = liga;
+    this.woj = woj;
+    this.stronaKlub = stronaKlub
 }
 
-const tabSamoloty=[];
-tabSamoloty [0]= new samolot(1, "737", 200, "Pasażerski", 2004, "https://www.boeing.com/");
-tabSamoloty [1]= new samolot(2, "Embraer 170", 150, "Pasażerski", 2002, "https://embraer.com/global/en");
-tabSamoloty [2]= new samolot(3, "Boeing 747", 400, "Pasażerski", 1969, "https://www.boeing.com/");
-tabSamoloty [3]= new samolot(4, "Airbus A380", 600, "Pasażerski", 2005, "https://www.airbus.com/en");
-tabSamoloty [4]= new samolot(5, "Boeing 777", 300, "Pasażerski", 1996, "https://www.boeing.com/");
-//console.log(ob_sam1);
+const tabKibice=[];
+tabKibice [0]= new kibice(1, "Arka Gdynia", 1929, "I liga", "Pomorskie", "http://www.arka.gdynia.pl/");
+tabKibice [1]= new kibice(2, "Cracovia", 1906, "Ekstraklasa", "Małopolskie", "https://cracovia.pl/");
+tabKibice [2]= new kibice(3, "ŁKS Łódź", 1908, "I liga", "Łódzkie", "https://lkslodz.pl/");
+tabKibice [3]= new kibice(4, "KSZO Ostrowiec Świętokrzyski", 1929, "III liga", "Świętokrzyskie", "https://kszo1929ostrowiec.pl/");
 
-//console.table(tabSamoloty);
+//console.log(ob_kib1);
+
+//console.table(tabKibice);
 
 let wyswietl = (tab)=> {
 
     let txt="";
     txt += `<tr>
-    <th> Id </th>
-    <th> Model </th>
-    <th> Liczba Pasażerów </th>
-    <th> Klasa </th>
-    <th> Rok </th>
-    <th> Strona Producenta </th>
+    <th> Lp. </th>
+    <th> Drużyna </th>
+    <th> Rok Założenia </th>
+    <th> Klasa rozgrywkowa </th>
+    <th> Wojewodztwo </th>
+    <th> Strona Klubu </th>
     </tr>`;
 
     tab.forEach(obiekt => {
         txt+= "<tr>";
-        txt+= "<td>" + obiekt.id + "</td>";       //dla for tab[i].id
-        txt+= "<td>" + obiekt.model + "</td>";
-        txt+= "<td>" + obiekt.liczbaPas + "</td>";
-        txt+= "<td>" + obiekt.klasa + "</td>";
+        txt+= "<td>" + obiekt.Lp + "</td>";       //dla for tab[i].id
+        txt+= "<td>" + obiekt.druzyna + "</td>";
         txt+= "<td>" + obiekt.rok + "</td>";
-        txt+= "<td><a href=" + obiekt.stronaProd +">" + "Strona www </a>" + "</td>";
+        txt+= "<td>" + obiekt.liga + "</td>";
+        txt+= "<td>" + obiekt.woj + "</td>";
+        txt+= "<td><a href=" + obiekt.stronaKlub +">" + "Strona www </a>" + "</td>";
         txt+= "</tr>";
         
     });
